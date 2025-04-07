1ebbd50f33dd4b7c63a6e678ae493883670b7ba3
Jenkins <rayjenkins820@gmail.com>
2025-04-07 16:08:31 +0300

    Моя реализация ip address

---
+++ data-structures/if-lookup/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "iflib.h"
+
+int main(int argc, char* argv[])
+{   
+    uint8_t ifamount = getNICAmount();
+    netifdata ifaddrs[ifamount];
+    getInterfacesAddr(ifaddrs, ifamount);
+    
+    for (int i = 0; i < ifamount; i++) 
+    {
+        if (ifaddrs[i].isActive)
+        {
+            printf("Interface (\033[32m\033[1m%s\033[0m):\n", ifaddrs[i].devname);   
+        }
+        else
+        {
+            printf("Interface (\033[31m\033[1m%s\033[0m):\n", ifaddrs[i].devname);
+        }
+    
+        printf("    ");
+        for (int j = 0; j < 6; j++) 
+        {
+            printf("%02x", ifaddrs[i].mac[j]);
+            if (j < 5) { printf(":"); }
+        }
+        
+        printf("\n    ");
+        for (int j = 0; j < 4; j++) 
+        {
+            printf("%d", ifaddrs[i].ipv4[j]);
+            if (j < 3) { printf("."); }
+        }
+    
+        printf("\n\n");
+    }
+
+    return 0;
+}
